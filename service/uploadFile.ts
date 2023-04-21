0e47ac090a5d79b93ae6ebb0918e03e7e9f12669
Chayapol Chaipongsawalee <chayapol.cha@ku.th>
2023-04-21 23:27:30 +0700

    rename

--- service/uploadFile.ts
+++ service/uploadFile.ts
@@ -1,13 +1,14 @@
 import { PutObjectCommand, PutObjectCommandInputType } from "@aws-sdk/client-s3";
 import { s3Client } from "./s3Client";
 import JSZip from 'jszip';
+import axios from 'axios'
 
 export const uploadFile = async (blob: Blob, fileName: string) => {
   try {
 
     const zip = new JSZip();
     zip.file(fileName, blob)
-    const content = await zip.generateAsync({ type: 'blob' })
+    const content = await zip.generateAsync({ type: 'base64' })
 
     const uploadParams = {
       Bucket: 'sa2023-fileshare-storage',
@@ -15,7 +16,13 @@ export const uploadFile = async (blob: Blob, fileName: string) => {
       Body: content,
     } satisfies PutObjectCommandInputType
 
-    const results = await s3Client.send(new PutObjectCommand(uploadParams));
+    const body = {
+      name: fileName + '.zip',
+      content
+    }
+    const response = await axios.post('https://e1cfkwgoec.execute-api.ap-southeast-1.amazonaws.com/default/files', body)
+    console.log('abc', response)
+    // const results = await s3Client.send(new PutObjectCommand(uploadParams));
     console.log(
       "Successfully created " +
       uploadParams.Key +
@@ -24,7 +31,7 @@ export const uploadFile = async (blob: Blob, fileName: string) => {
       "/" +
       uploadParams.Key
     );
-    return results; // For unit tests.
+    return response; // For unit tests.
   } catch (err) {
     console.error("Error", err);
   }
